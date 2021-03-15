27244131c26a148ca07e03343ffdcb5da1bc1d96
Saul <saul.moro.gomez@gmail.com>
2021-03-15 08:48:26 +0100

    fix(lib): remove rtk query import from public api v2

--- projects/ngrx-rtk-query/src/public-api.ts
+++ projects/ngrx-rtk-query/src/public-api.ts
@@ -1,8 +1,6 @@
 /*
  * Public API Surface of dialog
  */
-import { angularHooksModule, createApi } from './lib/module';
-import { QueryOptions } from './lib/types';
-
 export * from './lib/store-rtk-query.module';
-export { createApi, angularHooksModule, QueryOptions };
+export { createApi, angularHooksModule } from './lib/module';
+export { QueryOptions } from './lib/types';
