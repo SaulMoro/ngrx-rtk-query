390768874dc677eada33111af21c4b502a1a4c0a
Saul <saul.moro.gomez@gmail.com>
2021-03-31 13:22:14 +0200

    feat(lib): typescript 4-1 peer dependency and deep imports fix

--- projects/ngrx-rtk-query/src/public-api.ts
+++ projects/ngrx-rtk-query/src/public-api.ts
@@ -1,6 +1,6 @@
 /*
- * Public API Surface of dialog
+ * Public API Surface of ngrx-rtk-query
  */
 export * from './lib/store-rtk-query.module';
-export { createApi, angularHooksModule } from './lib/module';
+export * from './lib/module';
 export { QueryOptions, LazyQueryOptions } from './lib/types';
