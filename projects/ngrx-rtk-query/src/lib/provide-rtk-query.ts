7cbf7473a9bb536c3bad14926aa97a010755ea1d
Saul <saul.moro.gomez@gmail.com>
2023-01-31 20:28:59 +0100

    fix: circular dependency

--- projects/ngrx-rtk-query/src/lib/provide-rtk-query.ts
+++ projects/ngrx-rtk-query/src/lib/provide-rtk-query.ts
@@ -1,9 +1,6 @@
 import { EnvironmentProviders, importProvidersFrom } from '@angular/core';
 import { provideState } from '@ngrx/store';
-import { CoreModule } from '@reduxjs/toolkit/dist/query/core/module';
-import { Api } from '@reduxjs/toolkit/query';
 
-import { AngularHooksModule } from './module';
 import { StoreQueryConfig } from './store-rtk-query.config';
 import { StoreRtkQueryModule } from './store-rtk-query.module';
 
@@ -11,8 +8,6 @@ export function provideStoreRtkQuery(config?: Partial<StoreQueryConfig>): Enviro
   return importProvidersFrom(StoreRtkQueryModule.forRoot(config));
 }
 
-export function provideStoreApi(
-  api: Api<any, Record<string, any>, string, string, AngularHooksModule | CoreModule>,
-): EnvironmentProviders {
+export function provideStoreApi(api: any): EnvironmentProviders {
   return provideState(api.reducerPath, api.reducer, { metaReducers: [api.metareducer] });
 }
