764eb689a75777fd76165a81a3848a84fcefb6e0
Saul <saul.moro.gomez@gmail.com>
2023-02-21 12:35:42 +0100

    test: add new tests cases

--- src/app/features/skip/skip-container/skip-container.component.spec.ts
+++ src/app/features/skip/skip-container/skip-container.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { StoreModule } from '@ngrx/store';
 
 import { SkipContainerComponent } from './skip-container.component';
 
@@ -9,6 +10,7 @@ describe('SkipContainerComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [SkipContainerComponent],
+      imports: [StoreModule.forRoot({})],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SkipContainerComponent);
