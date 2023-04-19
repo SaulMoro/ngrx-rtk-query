9094d2e74d01312cec9be7d13dd8c0925f7020c1
Saul <saul.moro.gomez@gmail.com>
2023-04-19 10:35:36 +0200

    fix: update to rtk 1.9.5

--- projects/ngrx-rtk-query/jest.config.js
+++ projects/ngrx-rtk-query/jest.config.js
@@ -2,12 +2,24 @@ const baseConfig = require('../../jest.config');
 
 module.exports = {
   ...baseConfig,
-  globals: {
-    'ts-jest': {
-      tsconfig: './tsconfig.spec.json',
-    },
+  displayName: 'ngrx-rtk-query',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
   },
   moduleNameMapper: {
     'ngrx-rtk-query': '<rootDir>/src/public-api.ts',
   },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
 };
