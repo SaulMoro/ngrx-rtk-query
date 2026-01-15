624439104cfc53a97e72727040734fb42f29a8bf
Saul <saul.moro.gomez@gmail.com>
2026-01-15 10:56:33 +0100

    refactor: code structure for improved readability and maintainability

--- examples/basic-ngrx-store/vite.config.ts
+++ examples/basic-ngrx-store/vite.config.ts
@@ -1,33 +1,28 @@
-/// <reference types="vitest" />
+/// <reference types='vitest' />
 import angular from '@analogjs/vite-plugin-angular';
+import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  return {
-    root: __dirname,
-    cacheDir: '../../node_modules/.vite/examples/basic-ngrx-store',
-    plugins: [angular(), nxViteTsPaths()],
-    test: {
-      globals: true,
-      environment: 'jsdom',
-      setupFiles: ['src/test-setup.ts'],
-      include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-      passWithNoTests: true,
-      reporters: ['default'],
-      coverage: {
-        reportsDirectory: '../../coverage/examples/basic-ngrx-store',
-        provider: 'v8' as const,
-      },
+export default defineConfig(() => ({
+  root: __dirname,
+  cacheDir: '../../node_modules/.vite/examples/basic-ngrx-store',
+  plugins: [angular(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
+  // Uncomment this if you are using workers.
+  // worker: {
+  //   plugins: () => [ nxViteTsPaths() ],
+  // },
+  test: {
+    name: 'basic-ngrx-store',
+    watch: false,
+    globals: true,
+    environment: 'jsdom',
+    include: ['{src,tests}/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    setupFiles: ['src/test-setup.ts'],
+    reporters: ['default'],
+    coverage: {
+      reportsDirectory: '../../coverage/examples/basic-ngrx-store',
+      provider: 'v8' as const,
     },
-    define: {
-      'import.meta.vitest': mode !== 'production',
-    },
-    server: {
-      fs: {
-        allow: ['../..'],
-      },
-    },
-  };
-});
+  },
+}));
--- packages/ngrx-rtk-query/vite.config.ts
+++ packages/ngrx-rtk-query/vite.config.ts
@@ -1,31 +1,29 @@
 /// <reference types='vitest' />
 import angular from '@analogjs/vite-plugin-angular';
+import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(() => ({
   root: __dirname,
   cacheDir: '../../node_modules/.vite/packages/ngrx-rtk-query',
-  plugins: [angular(), nxViteTsPaths()],
+  plugins: [angular({ tsconfig: 'tsconfig.json' }), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
+  // Uncomment this if you are using workers.
+  // worker: {
+  //   plugins: () => [ nxViteTsPaths() ],
+  // },
   test: {
+    name: 'ngrx-rtk-query',
+    watch: false,
     globals: true,
     environment: 'jsdom',
+    include: ['{src,tests}/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     setupFiles: ['test-setup.ts'],
-    include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     passWithNoTests: true,
     reporters: ['default'],
     coverage: {
       reportsDirectory: '../../coverage/packages/ngrx-rtk-query',
-      provider: 'v8',
-    },
-  },
-  define: {
-    'import.meta.vitest': mode !== 'production',
-  },
-  server: {
-    fs: {
-      allow: ['../..'],
+      provider: 'v8' as const,
     },
   },
 }));
